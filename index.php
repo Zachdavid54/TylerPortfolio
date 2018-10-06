ce3acfa97e0db3058ca9fe609f2c63e7c622c3c3
Tyler Evans <tevs.creative@gmail.com>
2018-10-05 19:41:58 -0500

    Added new content to about section

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
 <?php include("includes/a_config.php");?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php include("includes/CSSInclude.php");?>
 </head>
+
 <body>
 
-<?php include("includes/design-top.php");?>
+    <?php include("includes/design-top.php");?>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="order-sm-2 col-sm-4 bg-primary-1 sidebar">
-            <div class="row">
-                <div class="col-md-12 p-md">
-                    <div class="logo">
-                        <a href="#"><img src="Images/tevs-logo.svg" alt="logo"></a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="order-sm-2 col-sm-4 bg-primary-1 sidebar">
+                <div class="row">
+                    <div class="col-md-12 p-md">
+                        <div class="logo">
+                            <a href="#"><img src="Images/tevs-logo.svg" alt="logo"></a>
+                        </div>
+
+                        <?php include("includes/navigation.php");?>
+
+                        <p class="m-top-md">Hello, my name is Tyler Evans. My goal as a UI/UX Designer is to create
+                            memorable digital
+                            experiences. Experiences that are user friendly &amp; long lasting.</p>
+                        <p>This is so cool!</p>
+                        <a href="mailto:tevs.creative@gmail.com" class="btn btn-primary m-top-md">Get In Touch</a>
                     </div>
-
-                    <?php include("includes/navigation.php");?>
-
-                    <p class="m-top-md">Hello, my name is Tyler Evans. My goal as a UI/UX Designer is to create
-                        memorable digital
-                        experiences. Experiences that are user friendly &amp; long lasting.</p>
-                    <a href="mailto:tevs.creative@gmail.com" class="btn btn-primary m-top-md">Get In Touch</a>
                 </div>
             </div>
+            <?php include("includes/Posters.php");?>
         </div>
-        <?php include("includes/Posters.php");?>
     </div>
-</div>
-<?php include("includes/JSInclude.php");?>
-<?php include("includes/footer.php");?>
+    <?php include("includes/JSInclude.php");?>
+    <?php include("includes/footer.php");?>
 
 </body>
+
 </html>

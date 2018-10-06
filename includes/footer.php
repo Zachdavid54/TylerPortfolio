d59a09bd2c7f7a13b26308f981795f71f15a86b4
Tyler Evans <tevs.creative@gmail.com>
2018-10-05 20:36:34 -0500

    Added layout classes to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 <div class="footer">
-	&copy; <?php print date("Y");?>
+<div class="container-fluid text-center well-md bg-primary-2">
+	tevs creative &copy; <?php print date("Y");?>
+</div>
 </div>

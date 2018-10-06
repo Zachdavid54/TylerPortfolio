434ff27c8b9dd3fb63dcda0b167a6151f62d1e75
Zach <zachdavid54@gmail.com>
2018-10-05 20:35:43 -0400

    Change About to Work

--- includes/navigation.php
+++ includes/navigation.php
@@ -6,7 +6,7 @@ $ini = parse_ini_file('app.ini');
         <a class="nav-link active" <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="<?php echo $ini['base_url']; ?>/index.php">Home</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link <?php if ($CURRENT_PAGE == "Work") {?>active<?php }?>" href="<?php echo $ini['base_url']; ?>/work.php">About Us</a>
+        <a class="nav-link <?php if ($CURRENT_PAGE == "Work") {?>active<?php }?>" href="<?php echo $ini['base_url']; ?>/work.php">Work</a>
     </li>
     <li class="nav-item">
         <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="<?php echo $ini['base_url']; ?>/contact.php">Contact</a>

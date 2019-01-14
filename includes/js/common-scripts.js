4269f0a8866e0ed6bcffa17e19b9740eb1aaaf5c
Tyler Evans <tevs.creative@gmail.com>
2019-01-13 22:29:40 -0600

    added markup for fancybox lightbox plugin

--- includes/js/common-scripts.js
+++ includes/js/common-scripts.js
@@ -3,19 +3,106 @@ $(document).ready(function () {
 		property: 'min-height'
   });
   
-  $('[data-fancybox="portfolio"]').fancybox({
-    // Options will go here
-     baseTpl:
-       '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-       '<div class="fancybox-bg"></div>' +
-       '<div class="fancybox-inner">' +
-       '<div class="fancybox-infobar"><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span></div>' +
-       '<div class="fancybox-toolbar">{{buttons}}</div>' +
-       '<div class="fancybox-navigation">{{arrows}}</div>' +
-       '<div class="fancybox-stage"></div>' +
-       '<div class="fancybox-caption"></div>' +
-       "</div>" +
-       "</div>",
+  // $('[data-fancybox="portfolio"]').fancybox({
+  //    baseTpl:
+  //      '<div class="fancybox-container" role="dialog" tabindex="-1">' +
+  //      '<div class="fancybox-bg"></div>' +
+  //      '<div class="fancybox-inner">' +
+  //      '<div class="fancybox-infobar"><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span></div>' +
+  //      '<div class="fancybox-toolbar">{{buttons}}</div>' +
+  //      '<div class="fancybox-navigation">{{arrows}}</div>' +
+  //      '<div class="fancybox-stage"></div>' +
+  //      '<div class="fancybox-caption"></div>' +
+  //      "</div>" +
+  //      "</div>",
+  // });
+
+  /*
+ 
+     Creative example - Product quick view
+       =====================================
+ 
+   */
+
+  $(".quick_view").fancybox({
+    baseClass: "quick-view-container",
+    infobar: false,
+    buttons: false,
+    thumbs: false,
+    margin: 0,
+    touch: {
+      vertical: false
+    },
+    animationEffect: false,
+    transitionEffect: "slide",
+    transitionDuration: 500,
+    baseTpl:
+      '<div class="fancybox-container" role="dialog">' +
+      '<div class="quick-view-content">' +
+      '<div class="quick-view-carousel">' +
+      '<div class="fancybox-stage"></div>' +
+      "</div>" +
+      '<div class="quick-view-aside"></div>' +
+      '<button data-fancybox-close class="quick-view-close">X</button>' +
+      "</div>" +
+      "</div>",
+
+    onInit: function (instance) {
+      /*
+
+			    #1 Create bullet navigation links
+			    =================================
+
+			*/
+
+      var bullets = '<ul class="quick-view-bullets">';
+
+      for (var i = 0; i < instance.group.length; i++) {
+        bullets += '<li><a data-index="' + i + '" href="javascript:;"><span>' + (i + 1) + "</span></a></li>";
+      }
+
+      bullets += "</ul>";
+
+      $(bullets)
+        .on("click touchstart", "a", function () {
+          var index = $(this).data("index");
+
+          $.fancybox.getInstance(function () {
+            this.jumpTo(index);
+          });
+        })
+        .appendTo(instance.$refs.container.find(".quick-view-carousel"));
+
+      /*
+
+			    #2 Add product form
+			    ===================
+
+			*/
+
+      var $element = instance.group[instance.currIndex].opts.$orig;
+      var form_id = $element.data("qw-form");
+
+      instance.$refs.container.find(".quick-view-aside").append(
+        // In this example, this element contains the form
+        $("#" + form_id)
+          .clone(true)
+          .removeClass("hidden")
+      );
+    },
+
+    beforeShow: function (instance) {
+      /*
+			    Mark current bullet navigation link as active
+			*/
+
+      instance.$refs.container
+        .find(".quick-view-bullets")
+        .children()
+        .removeClass("active")
+        .eq(instance.currIndex)
+        .addClass("active");
+    }
   });
 
 });
--- index.php
+++ index.php
@@ -56,6 +56,26 @@
           <div class="item">
             <img src="images/portfolio-items/posters/RickS_Poster.jpg" alt="portfolio poster">
           </div>
+          <p>
+            <a href="/images/portfolio-items/posters/Abeka_Poster.jpg" class="btn btn-primary quick_view m-top-md" data-fancybox="quick-view" data-qw-form="qw-form-1">
+            Open demo</a>
+            <span class="hidden">
+              <a class="quick_view" data-fancybox="quick-view" href="images/portfolio-items/posters/Ads_Poster.png">#2</a>
+              <a class="quick_view" data-fancybox="quick-view" href="images/portfolio-items/posters/PG_Poster.jpg">#3</a>
+            </span>
+          </p>
+          <div id="qw-form-1" class="hidden">
+            <h3>Some Fancy Dress</h3>
+            <p>
+              There should be a price tag and a brief description of the product.
+            </p>
+            <p>
+              Also, a form where customers could, for example, choose product size, color and quantity.
+            </p>
+            <p>
+              <button class="btn btn-primary" data-fancybox-close="">Add to cart</button>
+            </p>
+          </div>
         </div>
       </div>
     </div>
